e9f64a7b277020f91a08143f44330a195149f313
John Cho <johnycho16@gmail.com>
2019-02-25 11:32:15 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,11 @@ def countdown(int)
   "HAPPY NEW YEAR!"
 end
 
+def countdown_with_sleep(int)
+  while int > 0
+    puts "#{int} SECOND(S)!"
+    int -= 1
+    sleep 1
+  end
+  "HAPPY NEW YEAR!"
+end
